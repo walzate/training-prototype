fa7af426db00fc22d87ad635257977fac7e8eece
Wilson Alzate Calderon <wilson.alzate@payulatam.com>
2016-09-26 11:36:07 -0500

    Removed previous ways to inject the spring service without the sercice locator class.

--- src/main/java/com/payulatam/controller/ClientController.java
+++ src/main/java/com/payulatam/controller/ClientController.java
@@ -1,10 +1,8 @@
 package com.payulatam.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
-import org.zkoss.zkplus.spring.SpringUtil;
 import org.zkoss.zul.Label;
 import org.zkoss.zul.api.Textbox;
 
@@ -37,25 +35,19 @@ public class ClientController extends GenericForwardComposer {
 	/**
 	 * 
 	 */
-	private Label helloLabel;
+	private Label messageLabel;
 	/**
 	 * Reference to the client telephone text box
 	 */
 	private Textbox clientTelephoneTextBox;
-
 	/**
-	 * Spring context object
+	 * Client service instance
 	 */
-	private ApplicationContext context;
-
 	private ClientService clientService;
-
 	/**
-	 * Constructor for the class used to initialize the space
+	 * Logging manager
 	 */
-	public ClientController() {
-
-	}
+	final Logger LOGGER = Logger.getLogger(ClientController.class);
 
 	/**
 	 * Method used to listen the onclick event from the create client button. It
@@ -66,21 +58,15 @@ public class ClientController extends GenericForwardComposer {
 		Client client = buildClient();
 
 		clientService = ServiceLocator.getClientService();
-		
-		//clientService = (ClientService) SpringUtil.getBean("clientServiceImpl", ClientService.class);
-
-		// context = new
-		// ClassPathXmlApplicationContext(PrototypeConstants.SPRING_CONFIGURATION_FILE);
-		//
-		// clientService = (ClientService) context.getBean(ClientService.class);
 
 		try {
 			clientService.saveOrUpdate(client);
 		} catch (Exception e) {
-			e.printStackTrace();
+			LOGGER.error(e);
+			messageLabel.setValue("Error al crear el cliente");
 		}
 
-		helloLabel.setValue("Hello: " + client.toString());
+		messageLabel.setValue("Cliente creado exitosamente.");
 
 	}
 
