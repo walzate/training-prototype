baa61ee6c9876e6092b032d70100942fe25c9ffe
Wilson Alzate Calderon <wilson.alzate@payulatam.com>
2016-09-26 11:50:32 -0500

    Calling the space from the Spring configuration file instead of calling it programatically.

--- src/main/java/com/payulatam/dao/impl/ClientDaoImpl.java
+++ src/main/java/com/payulatam/dao/impl/ClientDaoImpl.java
@@ -1,11 +1,9 @@
 package com.payulatam.dao.impl;
 
 import org.openspaces.core.GigaSpace;
-import org.openspaces.core.GigaSpaceConfigurer;
-import org.openspaces.core.space.EmbeddedSpaceConfigurer;
+import org.openspaces.core.context.GigaSpaceContext;
 import org.springframework.stereotype.Component;
 
-import com.payulatam.constants.PrototypeConstants;
 import com.payulatam.dao.ClientDao;
 import com.payulatam.model.Client;
 
@@ -15,22 +13,15 @@ public class ClientDaoImpl implements ClientDao {
 	/**
 	 * Reference to the Gigaspaces space
 	 */
+	@GigaSpaceContext
 	private GigaSpace gigaSpace;
-	
+
 	/*
 	 * (non-Javadoc)
 	 * 
 	 * @see com.payulatam.dao.ClientDao#saveOrUpdate(com.payulatam.model.Client)
 	 */
 	public void saveOrUpdate(Client client) throws Exception {
-		findSpace();
 		gigaSpace.write(client);
 	}
-	
-	public void findSpace() {
-		EmbeddedSpaceConfigurer configurer = new EmbeddedSpaceConfigurer(PrototypeConstants.SPACE_NAME);
-		// Create the Space
-		gigaSpace = new GigaSpaceConfigurer(configurer).gigaSpace();
-	}
-
 }
